cf45c6100391bbe9e3baf48726aced1460281f1e
Thiago Hernandes <hernandes_sp@yahoo.com.br>
2021-04-01 09:17:49 -0300

    Criação do projeto

---
+++ src/main/java/com/activemq/PlayingActivemqSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.activemq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayingActivemqSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayingActivemqSpringBootApplication.class, args);
+	}
+
+}
